e3f213c39a8593cd829e40bac83139fa59d1f684
pwmurphy89 <pwmurphy89@gmail.com>
2018-04-06 14:20:57 -0500

    object practice

---
+++ main.js
@@ -0,0 +1,142 @@
+const candidateImages = Object.create({},{
+    enumerable: {
+        value: true
+    },
+    head: {
+        value: "adsfkj"
+    },
+    family: {
+        value: "dkl;afj"
+    }, 
+    constituents: {
+        value: "klj;"
+    }
+});
+const candidateVolunteer = Object.create({},{
+    enumerable: {
+        value: true
+    },
+    name: {
+        value: "John"
+    },
+    address: {
+        value: "3233 NSS"
+    },
+    email: { 
+        value: "volunteer@vol.net"
+    },
+    phone: {
+        value: "23432423423"
+    },
+    availability: {
+        value: "Always"
+    },
+    activities: {
+        value: "anything"
+    }
+});
+const candidateStatement = Object.create({}, {
+    enumerable: {
+        value: true
+    },
+    writable: {
+        value: true
+    },
+    taxes: {
+        writable: true,
+        value: "low"
+    },
+    jobs: {
+        value: "lots"
+    },
+    healthcare: {
+        value: "free"
+    },
+    crime: {
+        value: "none"
+    }
+});
+const poliCand = Object.create({}, {
+    district: {
+        writable: true,
+        enumerable: true,
+        value: "2nd Tennessee"
+    },
+    statements: {
+        enumerable: true,
+        value: candidateStatement
+    },
+    url_donate: {
+        enumerable: true,
+        value: "donateurl.net",
+    },
+    events: {
+        enumerable: true,
+        value: "all these events!"
+    },
+    volunteer: {
+        enumerable: true,
+        value: candidateVolunteer
+    },
+    biography: {
+        enumerable: true,
+        value: "from here"
+    },
+    images: {
+        enumerable: true,
+        value: candidateImages
+    },
+    mission: {
+        enumerable: true,
+        value: "alkdjfdls"
+    },
+    url_vote: {
+        enumerable: true,
+        value: "alkdsf.com"
+    }
+});
+
+
+function changeCandidateStatement(object, property, change){
+    object.property = change;
+}
+function changeCandidateVolunteer(candObject, property, change){
+    candidateVolunteer.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+function changeCandidateStatement(candObject, property, change){
+    poliCand.property = change;
+}
+
+
+
+
